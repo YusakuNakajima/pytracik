2a59e0fa8e05f1f6bba8a04cf6dbc13987950437
YusakuNakajima <yusaku_nakajima@ap.eng.osaka-u.ac.jp>
2025-05-07 22:15:31 +0900

    Update

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 # local install nlopt (for using newest version) 
+mkdir tmp
+cd tmp
 curl -O https://codeload.github.com/stevengj/nlopt/tar.gz/v2.7.0 && tar xzvf v2.7.0 && cd nlopt-2.7.0
-cmake . && make && sudo make install
-
+cmake . && make && make install
+cd ../../
+rm -r tmp
 # install pip packages
 pip install -r requirements.txt
 pip install -e .

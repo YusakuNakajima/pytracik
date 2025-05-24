b3ae0632980f15b72f46dbd0a337e2c3a2291220
YusakuNakajima <yusaku_nakajima@ap.eng.osaka-u.ac.jp>
2025-05-24 16:57:09 +0900

    Update

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
-# local install nlopt (for using newest version) 
+# local install nlopt (for using newest version)
 mkdir tmp
 cd tmp
 curl -O https://codeload.github.com/stevengj/nlopt/tar.gz/v2.7.0 && tar xzvf v2.7.0 && cd nlopt-2.7.0
-cmake . && make && make install
+# The following commands modify system directories, so they need sudo
+sudo cmake .
+sudo make
+sudo make install
 cd ../../
-rm -r tmp
-# install pip packages
-pip install -r requirements.txt
-pip install -e .
+sudo rm -r tmp
+# install pip packages (assuming you are in a virtual environment as per your previous request)
+python -m pip install -r requirements.txt
+python -m pip install -e .
